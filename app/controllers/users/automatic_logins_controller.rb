f602e85dab007b1a6458e5038bbb97b440198b38
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-05-09 08:41:12 -0700

    try ahoy queue again

--- app/controllers/users/automatic_logins_controller.rb
+++ app/controllers/users/automatic_logins_controller.rb
@@ -1,8 +1,15 @@
 class Users::AutomaticLoginsController < ApplicationController
+  rescue_from JWT::ExpiredSignature, with: :signature_expired
   def new
     decoded = Jwt::Decode.new(params[:jwt])
     @payload = decoded.result[0]
     @user = User.find_by_member_id(@payload['usr'])
     sign_in_and_redirect @user
+  end
+
+  private
+
+  def signature_expired
+    redirect_to root_path, notice: 'This link has expired.'
   end
 end
