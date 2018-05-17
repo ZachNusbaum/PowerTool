fcdec86ac8bd36b5c5ee8a984cbe6ae90f770d6d
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-05-16 18:10:27 -0700

    use vips instead of minimagick

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module PowerTools
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.active_storage.variant_processor = :vips
+
     # Enable/disable generators.
     config.generators do |g|
       # Core Rails
