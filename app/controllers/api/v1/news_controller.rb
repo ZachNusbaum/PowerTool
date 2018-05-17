ec3e71b29713c7312ce1f2742965d9b9ab219cff
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-05-17 13:59:11 -0700

    add oauth to menu and bookmarks api endpoint now shows stories instead bookmark table

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::NewsController < Api::BaseController
   before_action -> { doorkeeper_authorize! :read_all }
   def bookmarks
-    render json: current_user.bookmarks
+    render json: current_user.top_stories.select(:token, :source_id, :author, :description,
+                                                :story_url, :published_at)
   end
 end
