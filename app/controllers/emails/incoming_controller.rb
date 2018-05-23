b2f21c4f9c7c38751978acb38df577fcd7c0db1b
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-05-23 16:39:08 -0700

    fix incoming email endpoint

--- app/controllers/emails/incoming_controller.rb
+++ app/controllers/emails/incoming_controller.rb
@@ -1,4 +1,5 @@
 class Emails::IncomingController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
     puts params
     render json: { status: 'ok' }, status: :ok
