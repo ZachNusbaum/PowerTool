0c52280b1b8423bef3f2438bcb7627ee828ad1d9
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-05-30 16:39:16 -0700

    clear duplictaes when fetching new stories

--- app/jobs/get_top_stories_job.rb
+++ app/jobs/get_top_stories_job.rb
@@ -3,5 +3,14 @@ class GetTopStoriesJob < ApplicationJob
 
   def perform(*args)
     News::PopulateTopStories.run!
+    clear_duplicates
+  end
+
+  def clear_duplicates
+    duplicates = News::TopStory.select(:title, :story_url).group(:title, :story_url).having("count(*) > 1")
+    duplicates.each do |duplicate|
+      stories = News::TopStory.where(title: duplicate.title, story_url: duplicate.story_url)
+      stories.first.destroy if (stories.count > 1)
+    end
   end
 end
